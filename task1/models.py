97f2c0e2f8e06e592deadc737adea73a90b2413c
yaNick811 <167251929+yaNick811@users.noreply.github.com>
2024-12-18 14:43:38 +0300

    Add files via upload

---
+++ task1/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)  # Имя покупателя
+    balance = models.DecimalField(max_digits=10, decimal_places=2)  # Баланс
+    age = models.PositiveIntegerField()  # Возраст
+
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)  # Название игры
+    cost = models.DecimalField(max_digits=10, decimal_places=2)  # Цена
+    size = models.DecimalField(max_digits=10, decimal_places=2)  # Размер файлов игры
+    description = models.TextField()  # Описание
+    age_limited = models.BooleanField(default=False)  # Ограничение возраста 18+
+    buyer = models.ManyToManyField(Buyer, related_name='games')  # Покупатели игры
+
+    def __str__(self):
+        return self.title
+
+class News(models.Model):
+    title = models.CharField(max_length=200)  # Заголовок новости
+    content = models.TextField()  # Содержимое новости
+    date = models.DateTimeField(auto_now_add=True)  # Дата создания новости
+
+    def __str__(self):
+        return self.title
